189e5696b6f1ac2a33feacf5a61ca62e7b38a9e5
Kushagrashrivastava <kushagra10june@gmail.com>
2023-11-24 01:06:13 +0530

    deploy chrome extension

--- 5. Build a Chrome Extension/26. Make the template string even more dynamic/index.js
+++ 5. Build a Chrome Extension/26. Make the template string even more dynamic/index.js
@@ -4,6 +4,10 @@ const recipient = "James"
 // Create a new variable, sender, and set its value to your name
 let sender = "Kushagra"
 // Use your sender variable instead of "Per"
-const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
+const email = `Hey,
+ ${recipient}!
+  How is it going?
+   Cheers
+    ${sender}`
 
 console.log(email)
